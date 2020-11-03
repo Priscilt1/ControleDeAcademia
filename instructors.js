a09056b329a46524a2cd52a5493704ec1d0bfc91
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-11-03 20:21:06 -0300

    fazendo ajustes de datas, configuranção para datas universais e formulando datas para o formulario

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 const Intl = require('intl')
 
 
@@ -73,5 +73,10 @@ exports.edit = function(req,res) {
 
     if (!foundInstructor) return res.send("Instrutor não encontrado!")
 
-    return res.render('instructors/edit', { instructor: foundInstructor })
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', {instructor})
 }
--- utils.js
+++ utils.js
@@ -1,15 +1,31 @@
 module.exports = {
-    age: function (timestamp) {
-        const today = new Date()
-        const birthDate = new Date(timestamp)
-    
-        let age = today.getFullYear() - birthDate.getFullYear()
-        const month = today.getMonth() - birthDate.getMonth()
-    
-        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
-            age = age - 1
-        }
-    
-        return age
+  age: function (timestamp) {
+    const today = new Date();
+    const birthDate = new Date(timestamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+
+    if (month < 0 || (month == 0 && today.getDate() < birthDate.getDate())) {
+      age = age - 1;
     }
+
+    return age;
+  },
+
+  // convertendo o formato de milisegundo por yyyy-mm-dd   
+  date: function(timestamp) {
+    //date(instructor.birth)
+    const date = new Date (timestamp)  
+    //instructor.birth = 09329832837287483748739483
+    //date(instructor.bith)
+    //return yyyy-mm-dd
+
+    //o UTC deixa a data universal 
+    const year = date.getUTCFullYear()
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+    const day = `0${date.getUTCDate ()}`.slice(-2)
+
+    return `${year}-${month}-${day}`
+  }
 }
