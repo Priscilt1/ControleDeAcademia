0c108434590ba15871c9a1e3855c0f22cdc884cd
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-11-09 22:35:29 -0300

    editando membros

--- controllers/members.js
+++ controllers/members.js
@@ -70,7 +70,7 @@ exports.edit = function(req, res) {
 
     const member = {
         ...foundMember,
-        birth: date(foundMember.birth)
+        birth: date(foundMember.birth).iso
     }
 
     return res.render('members/edit', {member})
